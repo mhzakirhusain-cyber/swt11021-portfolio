f9982c883a9feeca7a922164644a6d68eaa1b2f3
zakir husain <mhzakirhusain@gmail.com>
2026-05-24 16:49:33 +0530

    Week 01: hello world, pipeline demo, greet, escape

---
+++ week01/Escape.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+
+    printf("Name:\tAbdul Haleem\n");
+    printf("Course:\tSWT 11012\n");
+    printf("Path:\tC:\\Users\\student\n\n");
+    printf("He said: \"Hello!\"\n");
+
+    return 0;
+}
---
+++ week01/greet.c/greet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{ 
+    printf("Good Morning =, Welcome to SEUSL world....|\n");
+    printf("this is the first c programming language in our class...\n");
+    printf ("Zakir Husain\n");
+    printf("SEU/IS/24/ICT/061");
+
+    return 0 ;
+
+}
